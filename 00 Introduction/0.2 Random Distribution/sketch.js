8dd6f293f2e13102740f849f3aebbac9ae376ed4
Shiva K <shivakansagara@gmail.com>
2023-06-25 11:02:37 -0700

    began 0.2

---
+++ 00 Introduction/0.2 Random Distribution/sketch.js
@@ -0,0 +1,15 @@
+// array to keep track of how often random numbers are picked
+let randomCounts = [];
+let total = 20;
+
+function setup() {
+  createCanvas(400, 400);
+  for(let i = 0; i < total; i++) {
+    randomCounts[i] = 0;
+  }
+}
+
+function draw() {
+  background(220);
+  let index = floor()
+}
